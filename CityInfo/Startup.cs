44dbe1521e455a184f492766d59bd12d71482507
jumpman <jbollman@protonmail.com>
2019-11-06 12:50:05 -0700

    init commit, first api completed!

---
+++ CityInfo/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CityInfo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();          //Adding the ASP.NEt Core MVC Middleware
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();      //Prod. This block will run if the aws env variable in VS was set from develop to prod.
+            }
+
+            app.UseMvc();
+            /*
+            app.Run(async (context) =>
+            {
+                //throw new Exception("stuff is broken");
+                await context.Response.WriteAsync("Hello Worldeee2.1!");
+            });
+            */
+        }
+    }
+}
