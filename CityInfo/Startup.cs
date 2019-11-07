ed5e2a48998c17963720e34f5f208e53c8379b27
jumpman <jbollman@protonmail.com>
2019-11-07 14:04:06 -0700

    added outputformatter code to allow xml to be returned.

--- CityInfo/Startup.cs
+++ CityInfo/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo
 {
@@ -15,7 +17,27 @@ namespace CityInfo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();          //Adding the ASP.NEt Core MVC Middleware
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlSerializerOutputFormatter()));
+            /*
+             Below is a fancy way of serializing the output. Meaning, i have the class attriubtes uppercase. By default, the output is in pascal case.
+             if i uncomment the below code, it will take it verbatim on how i wrote it. Only needed if it was a aestic requirement. Most likely
+             no one cares. 
+            
+            
+            .AddJsonOptions(o => {
+               if (o.SerializerSettings.ContractResolver != null)
+               {
+                    var castedResolver = o.SerializerSettings.ContractResolver
+                        as DefaultContractResolver;
+                    castedResolver.NamingStrategy = null;
+               }
+
+            });              //Adding the ASP.NEt Core MVC Middleware
+        */
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,15 +53,9 @@ namespace CityInfo
             }
 
             app.UseStatusCodePages();        // status code page middleware. simple text-based handler added to the request pipeline.
-            //This produces a status code 404 not found, because we dont have a startup page.
+                                            //This produces a status code 404 not found, because we dont have a startup page.
             app.UseMvc();
-            /*
-            app.Run(async (context) =>
-            {
-                //throw new Exception("stuff is broken");
-                await context.Response.WriteAsync("Hello Worldeee2.1!");
-            });
-            */
+
         }
     }
 }
