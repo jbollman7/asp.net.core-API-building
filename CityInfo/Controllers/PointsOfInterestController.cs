bea7c12f95ad8d3df0884fc741dee5582dc3ba33
jumpman <jbollman@protonmail.com>
2019-11-08 14:15:08 -0700

    first POST API complete!

--- CityInfo/Controllers/PointsOfInterestController.cs
+++ CityInfo/Controllers/PointsOfInterestController.cs
@@ -19,7 +19,7 @@ namespace CityInfo.Controllers
             return Ok(city.PointsOfInterest);
         }
         
-        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        [HttpGet("{cityId}/pointsofinterest/{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             //var pointOfInterest = PointOfInterestDto.Cuu
@@ -34,6 +34,37 @@ namespace CityInfo.Controllers
                 return NotFound();
 
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost("{cityId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            // this block will contain the request body will contain the data for the point of interest.
+            // we want to deseraialize into a point of interest, for creation dto. 
+            // Thats what a from body attribute is for.
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+                return NotFound();
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+            
+            city.PointsOfInterest.Add((finalPointOfInterest));
+
+            return CreatedAtRoute("GetPointOfInterest", new 
+                { cityId = cityId, id = finalPointOfInterest.Id}, finalPointOfInterest);
         }
     }
 }
---
+++ CityInfo/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,8 @@
+namespace CityInfo.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
